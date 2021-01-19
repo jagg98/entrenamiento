9e1d31e3d2fff8cc78b0adb0be1cc70c7a2858d3
jagg98 <alejandrogarciagomeztst@gmail.com>
2021-01-18 21:30:48 -0500

    Primer commit

---
+++ Programas/index.js
@@ -0,0 +1,15 @@
+"use stric"
+
+const chalk = require("chalk")
+const mulPorDos = require('./utils/utils')
+console.log(chalk.blue("Hola mundo"))
+
+
+
+
+for (let i=0;i<=10;i++)
+{
+    console.log(mulPorDos.multiplicarPorDos2(i))
+    
+}
+
---
+++ Programas/utils/utils.js
@@ -0,0 +1,14 @@
+"use stric"
+
+function multiplicarPorDos(num)
+{
+    console.log(num*2)
+}
+
+const multiplicarPorDos2 = (num)=>num*2
+
+
+module.exports = {
+    multiplicarPorDos,
+    multiplicarPorDos2
+}
